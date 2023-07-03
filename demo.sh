d5f02eac9dbe4f7228ea2d20c3a128a7e193e03f
Yerong <yerong.li@outlook.com>
2023-07-03 07:06:50 -0500

    Upload script

---
+++ demo.sh
@@ -0,0 +1,3 @@
+python src/cli_demo.py \
+  --model_name_or_path THUDM/chatglm-6b \
+  --checkpoint_dir elon_musk/checkpoint-300
---
+++ train.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python src/train_sft.py \
+    --do_train \
+    --dataset elon_musk \
+    --finetuning_type lora \
+    --output_dir elon_musk \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 50 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --quantization_bit 4 \
+    --fp16
