d6e3d5a9507228c61f3f5be0be7d391736968277
Yerong <yerong.li@outlook.com>
2023-07-03 22:07:38 -0500

    Update the size

--- train.sh
+++ train.sh
@@ -3,13 +3,13 @@ python src/train_sft.py \
     --dataset elon_musk \
     --finetuning_type lora \
     --output_dir elon_musk \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 8 \
     --gradient_accumulation_steps 2 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --save_steps 50 \
     --learning_rate 5e-5 \
     --num_train_epochs 3.0 \
-    --fp16    
-    # --quantization_bit 4 \
+    --fp16 \
+    --quantization_bit 4
 
