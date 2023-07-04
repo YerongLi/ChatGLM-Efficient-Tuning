d52f2778d826ba32accfe4f04463f7df44055487
Yerong <yerong.li@outlook.com>
2023-07-04 03:21:41 -0500

    Stop steam cat

--- src/cli_demo.py
+++ src/cli_demo.py
@@ -80,7 +80,7 @@ def main():
             continue
 
         count = 0
-        for _, history in model.stream_chat(tokenizer, query, history=(buffered_history + history).[:-6], **generating_args.to_dict()):
+        for _, history in model.stream_chat(tokenizer, query, history=(buffered_history + history)[:-6], **generating_args.to_dict()):
             if stop_stream:
                 stop_stream = False
                 break
