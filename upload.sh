a9f5b7a1b8962db7db5942026bbd58e54bbb3e03
Yerong <yerong.li@outlook.com>
2023-07-05 23:35:28 -0500

    4 bit

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 python src/cli_demo.py \
   --model_name_or_path THUDM/chatglm-6b \
-  --checkpoint_dir harry_potter/checkpoint-950
+  --checkpoint_dir harry_potter/checkpoint-950 \
+  --quantization_bit 4
