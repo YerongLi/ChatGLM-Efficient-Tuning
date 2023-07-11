2f1850125f750064623e7fd70eb2edaa5fd9d5ed
Yerong <yerong.li@outlook.com>
2023-07-11 05:20:44 -0500

    fastllm

--- demo.sh
+++ demo.sh
@@ -1,4 +1,5 @@
 python src/cli_demo.py \
   --model_name_or_path THUDM/chatglm-6b \
-  --checkpoint_dir harry_potter/checkpoint-700 \
   --quantization_bit 4
+  # --checkpoint_dir harry_potter/checkpoint-700 \
+
