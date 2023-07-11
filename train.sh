8297a5ab21acbd4ee826b8c248e102af7cd4d1d9
Yerong <yerong.li@outlook.com>
2023-07-11 04:19:40 -0500

    1400

--- train.sh
+++ train.sh
@@ -1,15 +1,16 @@
 python src/train_sft.py \
     --do_train \
+    --checkpoint_dir 'harry_potter/checkpoint-1400' \
     --dataset harry_potter \
     --finetuning_type lora \
     --output_dir harry_potter \
     --per_device_train_batch_size 10 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 4 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --save_steps 50 \
     --learning_rate 5e-5 \
-    --num_train_epochs 3.0 \
+    --num_train_epochs 10.0 \
     --fp16 \
     --quantization_bit 4
 
