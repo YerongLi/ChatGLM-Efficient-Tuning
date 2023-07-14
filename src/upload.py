908f36c939f77e92394373e7aa7047ea359ff6fd
Yerong <yerong.li@outlook.com>
2023-07-14 07:01:00 -0500

    Upload to CPU

--- src/upload.py
+++ src/upload.py
@@ -6,4 +6,6 @@ import platform
 from utils import prepare_infer_args, auto_configure_device_map, load_pretrained
 model_args, finetuning_args, generating_args = prepare_infer_args()
 model, tokenizer = load_pretrained(model_args, finetuning_args)
+model = model.to('cpu')
+print(model.parameters().__next__().device)
 model.push_to_hub("hp")
